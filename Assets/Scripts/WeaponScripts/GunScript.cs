e8cce34d8b9088dcd22d01c134eb94037efb6bee
09ejacob <elias.garseth.jacobsen@gmail.com>
2024-09-29 21:24:55 +0200

    Added a simple cooldown system for the gun script where the gun is limited to a set fire rate.

--- Assets/Scripts/WeaponScripts/GunScript.cs
+++ Assets/Scripts/WeaponScripts/GunScript.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class GunScript : MonoBehaviour {
-    [Tooltip("The movement controller for the weapon")]
+    [Tooltip("The controller for the weapon")]
     public WeaponController controller;
+
+    [Tooltip("The time between shots (in seconds)")]
+    public float fireRate = 1.0f;
+
+    private float nextFireTime = 0f;
 
     void Update() {
         Vector3 mouseDirecton = Input.mousePosition;
-
         controller.LookAtPoint(mouseDirecton);
 
-        if (Input.GetButtonDown("Fire1")) {
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextFireTime) { // Can also change the GetButtonDown to GetButton for auto fire
             controller.ApplyRecoil();
+            nextFireTime = Time.time + fireRate;
+            
+            Debug.Log("Fire");
         }
     }
 }
