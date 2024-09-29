7dd4c368b1ac1c62baa203c898732c61bc4a3359
09ejacob <elias.garseth.jacobsen@gmail.com>
2024-09-29 11:16:18 +0200

    Refactored LookAtPoint method. Moved it from PlayerMovement to PlayerController.

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask whatIsGround;
     [Tooltip("The position to check if the player is grounded")]
     [SerializeField] private Transform groundCheck;
+    [Tooltip("The gun object the player character will hold")]
+    public GameObject gun;
 
     private const float groundedRadius = .2f;
     private bool isGrounded;
@@ -45,7 +47,7 @@ public class PlayerController : MonoBehaviour
             else if (move < 0 && facingRight) Flip();
         }
 
-        if (isGrounded && jump) {
+        if (isGrounded && jump) { // Maybe make holding space jump higher
             isGrounded = false;
             rb.AddForce(new Vector2(0f, jumpForce));
         }
@@ -56,5 +58,10 @@ public class PlayerController : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    public void LookAtPoint(Vector3 point) { // Rotate the gun to look at the mouse
+        Vector2 lookDirection = Camera.main.ScreenToWorldPoint(point) - transform.position;
+        gun.transform.up = lookDirection;
     }
 }
--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     [Tooltip("The movement controller for the player")]
     public PlayerController controller;
 
@@ -13,31 +12,21 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("The movement speed")]
     public float moveSpeed = 40f;
 
-    [Tooltip("The gun object the player character will hold")]
-    public GameObject gun;
-
     bool jump = false;
     
-    void Update() // Get input
-    {
+    void Update() { // Get input
         horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        Vector3 mouseDirecton = Input.mousePosition;
 
-        LookAtPoint(Input.mousePosition);
+        controller.LookAtPoint(mouseDirecton);
 
-        if (Input.GetButtonDown("Jump"))
-        {
+        if (Input.GetButtonDown("Jump")) {
             jump = true;
         }
     }
 
-    void FixedUpdate() // FixedUpdate good for constant updating of movement. It is independent of framerate in contrast to Update
-    {
+    void FixedUpdate() { // FixedUpdate good for constant updating of movement. It is independent of framerate in contrast to Update
         controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
         jump = false;
-    }
-
-    public void LookAtPoint(Vector3 point) { // Rotate the gun to look at the mouse
-        Vector2 lookDirection = Camera.main.ScreenToWorldPoint(point) - transform.position;
-        gun.transform.up = lookDirection;
     }
 }
