a672da58c33282bd6533243ac0319d92766b8cec
09ejacob <elias.garseth.jacobsen@gmail.com>
2024-10-02 13:43:53 +0200

    Made it so that the gun sprite is flipped so that the weapon is always in the upwards position.

--- Assets/Scripts/WeaponScripts/GunScript.cs
+++ Assets/Scripts/WeaponScripts/GunScript.cs
@@ -4,17 +4,22 @@ public class GunScript : MonoBehaviour {
     [Tooltip("The controller for the weapon")]
     public WeaponController controller;
 
-    [Tooltip("The time between shots (in seconds)")]
-    public float fireRate = 1.0f;
-
     [Tooltip("Muzzleflash for the gun")]
     public ParticleSystem muzzleFlash;
+
+    [Tooltip("The sprite renderer for the gun")]
+    public SpriteRenderer gunSpriteRenderer;
+
+    [Tooltip("The time between shots (in seconds)")]
+    public float fireRate = 1.0f;
 
     private float nextFireTime = 0f;
 
     void Update() {
         Vector3 mouseDirection = Input.mousePosition;
         controller.LookAtPoint(mouseDirection);
+
+        FlipGunSprite();
 
         if (Input.GetButtonDown("Fire1") && Time.time >= nextFireTime) {
             controller.ApplyRecoil();
@@ -24,5 +29,17 @@ public class GunScript : MonoBehaviour {
 
             controller.sendRayCastAndPlayHitEffect();
         }
+    }
+
+    private void FlipGunSprite() {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        bool shouldFlip = mouseWorldPosition.x < controller.transform.position.x;
+
+        gunSpriteRenderer.flipY = shouldFlip;
+    }
+
+    public void FlipSpriteY(SpriteRenderer spriteRenderer) {
+        spriteRenderer.flipY = !spriteRenderer.flipY;
     }
 }
