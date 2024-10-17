14ef2597e029ee78c082bb642b029d29bf830de8
09ejacob <elias.garseth.jacobsen@gmail.com>
2024-10-17 10:47:50 +0200

    Added a pause screen which freezes the game and opens a pause menu. Made this a prefab. Also some small changes to main menu.

--- Assets/Scripts/UIMenu/UIMenu.cs
+++ Assets/Scripts/UIMenu/UIMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class UIMenu : MonoBehaviour
 {
     public void LoadLevel(string levelToLoad) {
         SceneManager.LoadScene(levelToLoad);
