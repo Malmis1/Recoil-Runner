3496d885a218a3f55093a94581a3312fd2c17422
09ejacob <elias.garseth.jacobsen@gmail.com>
2024-10-24 11:25:43 +0200

    Made the win menu contain the time the player used for that level.

--- Assets/Scripts/UIMenu/WinScript.cs
+++ Assets/Scripts/UIMenu/WinScript.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WinScript : MonoBehaviour {
     [Tooltip("The win menu object")]
     public GameObject winMenu;
 
+    public TextMeshProUGUI finalTimeText;
+
     public void PauseAndShowWinMenu() {
         Time.timeScale = 0;
         winMenu.SetActive(true);
+
+        SetFinalTimeText();
+    }
+
+    public void SetFinalTimeText() {
+        float timeSinceLoad = Time.timeSinceLevelLoad;
+
+        int seconds = Mathf.FloorToInt(timeSinceLoad);
+
+        int milliseconds = Mathf.FloorToInt((timeSinceLoad - seconds) * 100);
+
+        string timeString = string.Format("{0:00}:{1:00}", seconds, milliseconds);
+
+        if (finalTimeText != null) {
+            finalTimeText.text = timeString;
+        } else {
+            Debug.LogWarning("FinalTimeText is not assigned in the WinScript.");
+        }
     }
 }
