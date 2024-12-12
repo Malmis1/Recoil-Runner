142ae6bd280fe4eedf4fa90676ba05e212917cc3
Simonkho <63922818+Simonkrh@users.noreply.github.com>
2024-12-12 15:03:36 +0100

    Made quit button work on WebGL and in the Unity Editor

--- Assets/Scripts/UIMenu/UIMenu.cs
+++ Assets/Scripts/UIMenu/UIMenu.cs
@@ -73,7 +73,16 @@ public class UIMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit();
+#if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+        Debug.Log(this.name + " : " + this.GetType() + " : " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+#endif
+#if (UNITY_EDITOR)
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif (UNITY_STANDALONE)
+                            Application.Quit();
+#elif (UNITY_WEBGL)
+                            Application.OpenURL("https://simonkrh.itch.io/recoil-runner");
+#endif
     }
 
     public void OnPlayButtonPressed()
